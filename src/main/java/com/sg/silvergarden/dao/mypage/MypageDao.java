6cf6dbdf3a075e617d02478b145995c7ce5510ef
teddy5025 <teddy5025@naver.com>
2024-02-15 19:34:11 +0900

    마이페이지 branch 생성  태규 240215

---
+++ src/main/java/com/sg/silvergarden/dao/mypage/MypageDao.java
@@ -0,0 +1,10 @@
+package com.sg.silvergarden.dao.mypage;
+
+import java.util.List;
+import java.util.Map;
+
+public class MypageDao {
+    public List<Map<String, Object>> callMypage(Map<String, Object> mMap) {
+        return null;
+    }
+}
