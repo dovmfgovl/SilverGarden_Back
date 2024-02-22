4520d8c37f9d7e9ef16003597f465b87075ef928
suyeon <antndus2360@gmail.com>
2024-02-22 15:31:51 +0900

    security_suyeon_240222

--- src/main/java/com/sg/silvergarden/config/CorsConfiguration.java
+++ src/main/java/com/sg/silvergarden/config/CorsConfiguration.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 요청에 대해서
+        registry.addMapping("/**")
+                .allowedMethods("*")// 모든 요청에 대해서
                 .allowedOrigins("http://localhost:8000/","http://localhost:3000/"); // 허용할 오리진들
         //.allowedOrigins("*");
     }
