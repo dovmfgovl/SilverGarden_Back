0012f200bc3faaf326b9ad7396dcc37803c5ec91
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-02-23 12:53:38 +0900

    관리자-직원사진업로드 구현 중_이슬기_240223

--- src/main/java/com/sg/silvergarden/service/empinfo/EmpInfoService.java
+++ src/main/java/com/sg/silvergarden/service/empinfo/EmpInfoService.java
@@ -54,11 +54,21 @@ public class EmpInfoService {
         return result;
     }
 
+    @Transactional
     public int empEduUpdate(Map<String, Object> eMap) {
         logger.info("empEduUpdate");
         logger.info(eMap.toString());
         int result = 0;
-        result = empInfoDao.empEduUpdate(eMap);
+        if(eMap.containsKey("list")){//파일이 있는 경우 empUpdate를 먼저하고 시퀀스 값을 받아옴
+            List<Map<String, Object>> flist = (List)eMap.get("list");
+            empInfoDao.empUpdate(eMap);
+            for(Map<String, Object> map : flist){
+                map.put("E_NO", eMap.get("E_NO"));
+            }
+            result = empInfoDao.fileUpload(flist);
+        }else{
+            result = empInfoDao.empEduUpdate(eMap);
+        }
         return result;
     }
 
